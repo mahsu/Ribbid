a67cfb8e18b0900c74f6b4bc01fbb5e917500bc8
Austin Eng <213reeses@gmail.com>
2015-01-17 17:14:04 -0500

    geocode address

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -12,12 +12,30 @@ function RequestsController($scope, $http) {
 
   $scope.request_form = {};
   $scope.submitRequest = function(isValid) {
-    $scope.request_form.price = $scope.request_form.price.replace(/[^0-9.]+/g, "");
-    $scope.request_form.tags = $scope.request_form.tags.split(",");
-    console.log($scope.request_form);
-    $http.post('/api/requests', $scope.request_form)
-      .success(function(data) {
-        $location.path('/me/requests');
-      });
+    $scope.request_form.price = $scope.request_form.start_price.replace(/[^0-9.]+/g, "");
+    $scope.request_form.tags = $scope.request_form.cat_tags.split(",");
+    geoCode($scope.request_form.loc, function(pos) {
+      if (pos) {
+        $scope.request_form.address = $scope.request_form.loc;
+        $scope.request_form.location = {lat: pos.k, lon: pos.D}
+        console.log($scope.request_form);
+        $http.post('/api/requests', $scope.request_form)
+        .success(function(data) {
+          $location.path('/me/requests');
+        });
+      }
+    });
+
   };
+}
+
+function geoCode(address, callback) {
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      callback(results[0].geometry.location);
+    } else {
+      callback(false);
+    }
+  });
 }
